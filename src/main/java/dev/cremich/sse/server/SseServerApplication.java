c3018e3f03512136f1b9176c1f068d8dd6361dc4
Christian Bonzelet <christian.bonzelet@gmail.com>
2019-03-15 15:28:02 +0100

    setup spring-boot application

---
+++ src/main/java/dev/cremich/sse/server/SseServerApplication.java
@@ -0,0 +1,13 @@
+package dev.cremich.sse.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SseServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/cremich/sse/server/SseServerApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.cremich.sse.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SseServerApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
